3793010909618d4c855c210d1b6f398e3cdfde2c
shoh <shoh4486@naver.com>
2019-11-27 13:47:28 +0900

    commit v2.1

--- main.py
+++ main.py
@@ -44,105 +44,109 @@ flags.DEFINE_boolean('eval_with_test_acc', True, 'True for test accuracies evalu
 flags.DEFINE_integer('output_stride_testing', 8, 'output stride in the training mode')
 FLAGS = flags.FLAGS
 
-pprint.pprint(flags.FLAGS.__flags)
-
-mkdir(FLAGS.save_dir)
-mkdir(os.path.join(FLAGS.save_dir, "test"))
-
-if FLAGS.gpu_num: # gpu_num >= 1
-    run_config = tf.ConfigProto()
-    run_config.gpu_options.allow_growth = True
-    sess = tf.Session(config=run_config)
-else: # only cpu
-    sess = tf.Session()
-
-deeplabv3plus = DeepLabv3plus(
-                              sess=sess,
-                              C_in=FLAGS.C_in,
-                              num_class=FLAGS.num_class,
-                              separable_aspp_decoder=FLAGS.separable,
-                              seed=FLAGS.seed,
-                              weight_decay_lambda=FLAGS.weight_decay_lambda,
-                              truncated=FLAGS.truncated,
-                              optimizer=FLAGS.optimizer,
-                              save_dir=FLAGS.save_dir,
-                              gpu_num=FLAGS.gpu_num
-                              )
-
-global_variables_list()
-
-if FLAGS.train:
-    from data.data_preprocessing import inputs_train, inputs_train_, inputs_valid, gts_train, gts_train_, gts_valid
-    inputs_col = [inputs_train, inputs_train_, inputs_valid]
-    for i in inputs_col:
-        inputs_pixel_checker(i)
+def main(_):
+    pprint.pprint(flags.FLAGS.__flags)
+    
+    mkdir(FLAGS.save_dir)
+    mkdir(os.path.join(FLAGS.save_dir, "test"))
+    
+    if FLAGS.gpu_num: # gpu_num >= 1
+        run_config = tf.ConfigProto()
+        run_config.gpu_options.allow_growth = True
+        sess = tf.Session(config=run_config)
+    else: # only cpu
+        sess = tf.Session()
+    
+    deeplabv3plus = DeepLabv3plus(
+                                  sess=sess,
+                                  C_in=FLAGS.C_in,
+                                  num_class=FLAGS.num_class,
+                                  separable_aspp_decoder=FLAGS.separable,
+                                  seed=FLAGS.seed,
+                                  weight_decay_lambda=FLAGS.weight_decay_lambda,
+                                  truncated=FLAGS.truncated,
+                                  optimizer=FLAGS.optimizer,
+                                  save_dir=FLAGS.save_dir,
+                                  gpu_num=FLAGS.gpu_num
+                                  )
+    
+    global_variables_list()
+    
+    if FLAGS.train:
+        from data.data_preprocessing import inputs_train, inputs_train_, inputs_valid, gts_train, gts_train_, gts_valid
+        inputs_col = [inputs_train, inputs_train_, inputs_valid]
+        for i in inputs_col:
+            inputs_pixel_checker(i)
+            
+        gts_col = [set(gts_train.flatten()), set(gts_train_.flatten()), set(gts_valid.flatten())]
+        for i in gts_col:
+            gts_pixel_checker(i)
         
-    gts_col = [set(gts_train.flatten()), set(gts_train_.flatten()), set(gts_valid.flatten())]
-    for i in gts_col:
-        gts_pixel_checker(i)
-    
-    if FLAGS.restore:
-        saver = tf.train.Saver()
-        saver.restore(sess, FLAGS.pre_train_dir)
-        FLAGS.start_epoch = FLAGS.restart_epoch
-        FLAGS.end_epoch = FLAGS.re_end_epoch
-        deeplabv3plus.train(
-                            inputs=(inputs_train, inputs_train_, inputs_valid),
-                            gts=(gts_train, gts_train_, gts_valid),
-                            config=FLAGS
-                            )
-        saver = tf.train.Saver()
-        saver.save(sess, os.path.join(FLAGS.save_dir, "sess"), global_step=FLAGS.end_epoch-1)
-    
-    else:  
-        deeplabv3plus.train(
-                            inputs=(inputs_train, inputs_train_, inputs_valid),
-                            gts=(gts_train, gts_train_, gts_valid),
-                            config=FLAGS
-                            )
-        saver = tf.train.Saver()
-        saver.save(sess, os.path.join(FLAGS.save_dir, "sess"), global_step=FLAGS.end_epoch-1)
-    
-    np.savetxt(os.path.join(FLAGS.save_dir, "CEE_train.txt"), deeplabv3plus.CEE_train_vals)
-    np.savetxt(os.path.join(FLAGS.save_dir, "miou_train.txt"), deeplabv3plus.miou_train_vals)
-    np.savetxt(os.path.join(FLAGS.save_dir, "PA_ALL_train.txt"), deeplabv3plus.PA_ALL_train_vals)
-    
-    np.savetxt(os.path.join(FLAGS.save_dir, "CEE_valid.txt"), deeplabv3plus.CEE_valid_vals)
-    np.savetxt(os.path.join(FLAGS.save_dir, "miou_valid.txt"), deeplabv3plus.miou_valid_vals)
-    np.savetxt(os.path.join(FLAGS.save_dir, "PA_ALL_valid.txt"), deeplabv3plus.PA_ALL_valid_vals)
-    
-else: # testing mode
-    try:
-        from data.test_data_preprocessing import inputs_test, gts_test  
-    except ImportError: # when gts_test is not given
-        from data.test_data_preprocessing import inputs_test
-    else:
-        gts_pixel_checker(set(gts_test.flatten()))
-    finally:
-        inputs_pixel_checker(inputs_test)
-    
-    if FLAGS.restore:
-        saver = tf.train.Saver()
-        saver.restore(sess, FLAGS.pre_train_dir)
-        if FLAGS.eval_with_test_acc:
-            test_results = deeplabv3plus.evaluation(
+        if FLAGS.restore:
+            saver = tf.train.Saver()
+            saver.restore(sess, FLAGS.pre_train_dir)
+            FLAGS.start_epoch = FLAGS.restart_epoch
+            FLAGS.end_epoch = FLAGS.re_end_epoch
+            deeplabv3plus.train(
+                                inputs=(inputs_train, inputs_train_, inputs_valid),
+                                gts=(gts_train, gts_train_, gts_valid),
+                                config=FLAGS
+                                )
+            saver = tf.train.Saver()
+            saver.save(sess, os.path.join(FLAGS.save_dir, "sess"), global_step=FLAGS.end_epoch-1)
+        
+        else:  
+            deeplabv3plus.train(
+                                inputs=(inputs_train, inputs_train_, inputs_valid),
+                                gts=(gts_train, gts_train_, gts_valid),
+                                config=FLAGS
+                                )
+            saver = tf.train.Saver()
+            saver.save(sess, os.path.join(FLAGS.save_dir, "sess"), global_step=FLAGS.end_epoch-1)
+        
+        np.savetxt(os.path.join(FLAGS.save_dir, "CEE_train.txt"), deeplabv3plus.CEE_train_vals)
+        np.savetxt(os.path.join(FLAGS.save_dir, "miou_train.txt"), deeplabv3plus.miou_train_vals)
+        np.savetxt(os.path.join(FLAGS.save_dir, "PA_ALL_train.txt"), deeplabv3plus.PA_ALL_train_vals)
+        
+        np.savetxt(os.path.join(FLAGS.save_dir, "CEE_valid.txt"), deeplabv3plus.CEE_valid_vals)
+        np.savetxt(os.path.join(FLAGS.save_dir, "miou_valid.txt"), deeplabv3plus.miou_valid_vals)
+        np.savetxt(os.path.join(FLAGS.save_dir, "PA_ALL_valid.txt"), deeplabv3plus.PA_ALL_valid_vals)
+        
+    else: # testing mode
+        try:
+            from data.test_data_preprocessing import inputs_test, gts_test  
+        except ImportError: # when gts_test is not given
+            from data.test_data_preprocessing import inputs_test
+        else:
+            gts_pixel_checker(set(gts_test.flatten()))
+        finally:
+            inputs_pixel_checker(inputs_test)
+        
+        if FLAGS.restore:
+            saver = tf.train.Saver()
+            saver.restore(sess, FLAGS.pre_train_dir)
+            if FLAGS.eval_with_test_acc:
+                test_results = deeplabv3plus.evaluation(
+                                                        inputs=inputs_test,
+                                                        output_stride=FLAGS.output_stride_testing,
+                                                        gts=gts_test
+                                                        )
+                seg_test, CEE_test, miou_test, PA_ALL_test = test_results
+                np.savetxt(os.path.join(FLAGS.save_dir, "test", "CEE_test.txt"), CEE_test)
+                np.savetxt(os.path.join(FLAGS.save_dir, "test", "miou_test.txt"), miou_test)
+                np.savetxt(os.path.join(FLAGS.save_dir, "test", "PA_ALL_test.txt"), PA_ALL_test)
+            
+            else:
+                seg_test = deeplabv3plus.evaluation(
                                                     inputs=inputs_test,
                                                     output_stride=FLAGS.output_stride_testing,
-                                                    gts=gts_test
+                                                    gts=None
                                                     )
-            seg_test, CEE_test, miou_test, PA_ALL_test = test_results
-            np.savetxt(os.path.join(FLAGS.save_dir, "test", "CEE_test.txt"), CEE_test)
-            np.savetxt(os.path.join(FLAGS.save_dir, "test", "miou_test.txt"), miou_test)
-            np.savetxt(os.path.join(FLAGS.save_dir, "test", "PA_ALL_test.txt"), PA_ALL_test)
-        
+            for i in range(len(seg_test)):
+                for c in range(FLAGS.num_class):
+                    np.savetxt(os.path.join(FLAGS.save_dir, "test", "test_result%d_class%d.txt" % (i, c)), seg_test[i, :, :, c])
         else:
-            seg_test = deeplabv3plus.evaluation(
-                                                inputs=inputs_test,
-                                                output_stride=FLAGS.output_stride_testing,
-                                                gts=None
-                                                )
-        for i in range(len(seg_test)):
-            for c in range(FLAGS.num_class):
-                np.savetxt(os.path.join(FLAGS.save_dir, "test", "test_result%d_class%d.txt" % (i, c)), seg_test[i, :, :, c])
-    else:
-        raise NotImplementedError('pretrained session must be restored.')
+            raise NotImplementedError('pretrained session must be restored.')
+            
+if __name__ == '__main__':
+    tf.app.run()
